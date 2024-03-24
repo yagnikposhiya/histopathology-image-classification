325b46c81d59f0d3c8f7376825d9e153f6446404
yagnikposhiya <yagnikposhiya97275@gmail.com>
2024-03-24 18:16:46 +0530

    added all existing version of pre-trained resnet in torchvision module

--- src/nn_arch/neural_network.py
+++ src/nn_arch/neural_network.py
@@ -5,10 +5,26 @@ organization: Charotar University of Science and Technology
 """
 
 from config import Config
-from torchvision.models import resnet18
+from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152
 
 config = Config() # create an object of class Config
 
 class Resnet18():
     def __init__(self):
         self.MODEL = resnet18(pretrained=False, num_classes=config.NUM_CLASSES) # did not use pretrained model
+
+class Resnet34():
+    def __init__(self):
+        self.MODEL = resnet34(pretrained=False, num_classes=config.NUM_CLASSES) # did not use pretrained model
+
+class Resnet50():
+    def __init__(self):
+        self.MODEL = resnet50(pretrained=False, num_classes=config.NUM_CLASSES) # did not use pretrained model
+
+class Resnet101():
+    def __init__(self):
+        self.MODEL = resnet101(pretrained=False, num_classes=config.NUM_CLASSES) # did not use pretrained model
+
+class Resnet151():
+    def __init__(self):
+        self.MODEL = resnet152(pretrained=False, num_classes=config.NUM_CLASSES) # did not use pretrained model       
